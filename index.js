dc51807312cfb0d568da6bd2d2fdc2f5362b36fa
Padmanabh2 <desaipadmanabh191@gmail.com>
2022-10-25 13:32:03 +0530

    adding project section

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 
-document.querySelector('.home').addEventListener('click',()=>{
+document.querySelector('#olxgit').addEventListener('click',()=>{
 
-    window.location.href='./index.html';
+    window.open('https://github.com/arnab9593/likeable-air-3346/tree/master/final_release','_blank');
+
+})
+document.querySelector('#olxdeploy').addEventListener('click',()=>{
+
+    window.open('https://charming-concha-ababfd.netlify.app/','_blank');
+
+})
+
+document.querySelector('#bitrixgit').addEventListener('click',()=>{
+
+    window.open('https://github.com/sombir5/affable-limit-5459','_blank');
+
+})
+document.querySelector('#bitrixdeploy').addEventListener('click',()=>{
+
+    window.open('https://spontaneous-halva-547015.netlify.app/','_blank');
+
+})
+
+document.querySelector('#espngit').addEventListener('click',()=>{
+
+    window.open('https://github.com/surajsharma83074/espncric.info','_blank');
+
+})
+document.querySelector('#espndeploy').addEventListener('click',()=>{
+
+    window.open('https://cerulean-starlight-6bf43f.netlify.app/','_blank');
 
 })
 
