70cd5bf256d038703ba53421d644f5f0c875c438
Padmanabh2 <desaipadmanabh191@gmail.com>
2022-11-02 00:06:26 +0530

    adding works

---
+++ project.js
@@ -0,0 +1,16 @@
+
+//   GitHubCalendar(".calendar", "Padmanabh2");
+
+//     // or enable responsive functionality:
+//     GitHubCalendar(".calendar", "Padmanabh2", { responsive: true });
+
+//     // Use a proxy
+//     GitHubCalendar(".calendar", "Padmanabh2", {
+//        proxy (username) {
+//          return fetch(`https://your-proxy.com/github?user=${username}`)
+//        }
+//     }).then(r => r.text())
+
+// GitHubCalendar(".calendar", "Padmanabh2");
+//        // or enable responsive functionality
+// GitHubCalendar(".calendar", "Padmanabh2", { responsive: true });
